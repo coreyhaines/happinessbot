145b5d2aedc06b75822b00c979a8f0115f527a6c
Mats Sigge <mats.sigge@gmail.com>
2011-12-23 09:53:48 +0100

    Added responses in Swedish

--- run_bot.rb
+++ run_bot.rb
@@ -35,7 +35,11 @@ bot = TweetBot.configure do |config|
     response << "आपको भी शुभ प्रभात । आपका िदन मंगलमय हो ।" << "यह एक अच्छी सुबह है. और दुनिया बेहतर है क्योंकि आप यहाँ हैं|" << "सुप्रभात ! आप आज एक शानदार दिन का आनंद लें ।" << "आप एक अद्भुत सुबह का आनंद लें भी । हर दिन नई संभावनाओं के साथ खुलता है ।" << "सुप्रभात । लाजवाब होने के लिए धन्यवाद, अपने दिन का आनंद लें ।" << "अपने दिन का आनंद लें । जीवन अद्भुत है, और आप इसे और भी बेहतर बनाएं |" << "मैं आशा करता हूं कि आपका दिन लाजवाब बीते । आप इसके पात्र हैं" << "मुझे आशा है कि आज आप के लिए अद्भुत है" << "हर दिन नई क्षमता और संभावनाओं से भरा है। आप उन्हें आनंद के अधिकारी हैं ।" << "अपनी सुबह का आनंद लें।एक गहरी साँस ले । याद रखें आप लाजवाब हैं!"
   end
 
-  # UTF errors
+    config.respond_to_phrase "god morgon" do |responses|
+    responses << "God morgon själv! Ha en fantastisk dag idag! Kom ihåg hur bra du är!" << "Det är en härlig morgon. Och världen är bättre för att du är här."<< "God morgon! Du förtjänar en fantastisk dag idag. Njut av den!" << "Och en härlig morgon till dig också! Varje dag gryr med nya möjligheter. Njut av den här!"<< "God morgon! Tack för att du är fantastisk. Njut av din dag!" << "Njut av din dag! Livet är härligt, och du gör det ännu bättre!" << "Jag hoppas din dag blir härlig. Det förtjänar du." << "Jag hoppas att idag blir en underbar dag för dig! Tack för att du är så bra!" << "Varje dag ger nya möjligheter. Du förtjänar att njuta av dem!" << "Ha en bra morgon! Slappna av. Andas djupt. Glöm inte bort att du är fantastisk!" << "God morgon! Idag blir en bra dag. Njut av den! Det förtjänar du."
+  end
+
+# UTF errors
   # config.respond_to_phrase "bom dia" do |responses|
     # responses << "E um bom dia pra você também! Tenha um fantastico dia hoje! Lembre que você é incrível!" << "É um bom dia. E o mundo está melhor porque você está aqui." << "Bom dia! Você merece um fantastico dia hoje! Aproveite!" << "E uma ótima manhã para você também! Cada dia traz novas possibilidades. Aproveite!" << "Bom dia! Obrigado por ser incrível. Aproveite seu dia" << "Aproveite o seu dia! A vida é incrível, e você deixa ela ainda melhor" << "Eu espero que o seu dia seja maravilhoso! Você merece!" << "Eu espero que o seu dia de hoje seja maravilhoso para você. Obrigado por ser incrível!" << "Cada dia possui um novo potencial e possibilidades. Você merece aproveita-las" << "Aproveite sua manhã! Relaxe e respire fundo. Lembre que você é incrível!"
   # end
