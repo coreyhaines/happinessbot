817f5ad260e477de9ec37461a775c63bb3dffa8f
coreyhaines <coreyhaines@gmail.com>
2012-01-08 10:38:25 -0800

    Disable spanish until Faraday gem updates to handle utf-8 warnings

--- run_bot.rb
+++ run_bot.rb
@@ -11,9 +11,6 @@ bot = TweetBot.configure do |config|
     responses << "And a good morning to you! Have a fantastic day today! Remember how awesome you are!" << "It is a good morning. And the world is better because you are here."<< "Good Morning! You deserve a fantastic day today! Enjoy!"<< "And a wonderful morning to you, too! Every day opens with new possibilities. Enjoy!"<< "Good Morning! Thanks for being awesome. Enjoy your day!" << "Enjoy your day! Life is great, and you make it even better!" << "I hope your day today is wonderful! You deserve it!" << "I hope today is wonderful for you. Thanks for being great!" << "Every day has new potential and possibilities. You deserve to enjoy them!" << "Enjoy your morning! Relax and take a deep breath. Remember you are awesome!"
   end
 
-  config.respond_to_phrase "buenos días" do |responses|
-    responses << "¡Buenos días para ti tambien! ¡Que tengas un fantastico día! ¡Recuerda lo asombroso que eres!" << "Son buenos días. Y el mundo es mejor porque estás en el."<< "Buenos Días! ¡Te mereces un fantastico día! ¡Disfruta!"<< "¡Una increible mañana para ti tambien! Cada día trae nuevas posibilidades. ¡Disfruta!"<< "¡Buenos Días! Gracias por ser genial. ¡Disfruta de tu día!" << "¡Disfruta ru día! ¡La vida es maravillosa, y tu la hace aún mejor!" << "¡Espero que tu día sea maravilloso! ¡Te lo mereces!" << "Espero que hoy sea un increíble día para ti. ¡Gracias por ser genial!" << "Cada día tiene nuevo potencial y posibilidades. ¡Mereces disfrutarlas!" << "¡Disfruta de tu mañana! Relajate y respira profundo. ¡Recuerda que eres asombroso!"
-  end
 
   config.respond_to_phrase "bonjour" do |responses|
     responses << "Bonjour à vous! C'est une journée fantastique aujourd'hui! Rappelez-vous comme vous êtes génial!" << "C'est une bonne matinée, car le monde est meilleur quand vous êtes ici." << "Bonjour! Vous méritez une journée fantastique aujourd'hui! Amusez-vous!"<< "Et un matin merveilleux pour vous aussi! Chaque jour ouvre de nouvelles possibilités. Amusez-vous!"<< "Bonjour! Merci d'être génial. Profitez de votre journée!" << "Profitez de votre journée! La vie est belle, et vous la rendez encore meilleure!" << "J'espère que votre journée sera merveilleuse! Vous le méritez!" << "J'espère qu'aujourd'hui sera une journée merveilleuse pour vous. Merci d'être là!" << "Chaque jour ouvre des nouvelles portes et vous méritez d'en profiter!" << "Profitez de votre matinée! Détendez-vous et prenez une profonde respiration. N'oubliez pas que vous êtes génial!"
@@ -58,7 +55,10 @@ config.respond_to_phrase "dzień dobry" do |responses|
   config.respond_to_phrase "صباح الخير" do |responses|
     responses << "صباح النور! يارب يومك يبقي هايل! افتكر اد ايه انت عاظيم!" << "انهاردة يوم جميل! والعالم احلي بوجودك."<< "صباح النور! انت تستحق يوم عاظيم انهاردة! استمتع!"<< "صباح النور! كل يوم بيفتح ابواب جديدة وفرص جديدة! استمتع!"<< "صباح النور! شكراً علي عظامتك. استمتع بيومك!" << "استمتع بيومك! الحياة حلوة، وانت بتحليها اكتر!" << "اتمني يومك يكون جميل! انت تستاهل!" << "اتمني يومك يكون عظيم! اشكراً لانك جميل!" << "كل يوم بيفتح ابواب جديدة وفرص جديدة! اانت من حقك تستمتع بيهم!" << "استمتع بيومك! استرخي وخد نفس عميق! افتكر اد ايه انت عظيم!"
   end
-  # UTF errors
+  # UTF errors Waiting for release of faraday gem to handle these
+  # config.respond_to_phrase "buenos días" do |responses|
+    # responses << "¡Buenos días para ti tambien! ¡Que tengas un fantastico día! ¡Recuerda lo asombroso que eres!" << "Son buenos días. Y el mundo es mejor porque estás en el."<< "Buenos Días! ¡Te mereces un fantastico día! ¡Disfruta!"<< "¡Una increible mañana para ti tambien! Cada día trae nuevas posibilidades. ¡Disfruta!"<< "¡Buenos Días! Gracias por ser genial. ¡Disfruta de tu día!" << "¡Disfruta ru día! ¡La vida es maravillosa, y tu la hace aún mejor!" << "¡Espero que tu día sea maravilloso! ¡Te lo mereces!" << "Espero que hoy sea un increíble día para ti. ¡Gracias por ser genial!" << "Cada día tiene nuevo potencial y posibilidades. ¡Mereces disfrutarlas!" << "¡Disfruta de tu mañana! Relajate y respira profundo. ¡Recuerda que eres asombroso!"
+  # end
 end
 
 bot.talk
