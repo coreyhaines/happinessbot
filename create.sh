5a3782db92bfb21a443edf33bd0afc1a759027e9
coreyhaines <coreyhaines@gmail.com>
2012-01-09 10:51:04 -0800

    Update script with pushing code to heroku before scaling up

--- create.sh
+++ create.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-ITERATION=9
-APP_INDEX=3
-CONSUMER_KEY=<key>
-CONSUMER_SECRET=<secret>
-OAUTH_TOKEN=<token>
-OAUTH_SECRET=<secret>
-heroku create hb$APP_INDEX -r bot_$APP_INDEX --stack cedar --addons apigee:basic
+ITERATION=10
+APP_INDEX=4
+CONSUMER_KEY=
+CONSUMER_SECRET=
+OAUTH_TOKEN=
+OAUTH_SECRET=
+heroku create hb$APP_INDEX -r bot_$ITERATION --stack cedar --addons apigee:basic
 heroku addons:upgrade logging:expanded --app hb$APP_INDEX
 heroku config:add BOT_NAME=happiness2_u_$ITERATION BOT_TWITTER_CONSUMER_KEY=$CONSUMER_KEY BOT_TWITTER_CONSUMER_SECRET=$CONSUMER_SECRET BOT_TWITTER_OAUTH_TOKEN=$OAUTH_TOKEN BOT_TWITTER_OAUTH_TOKEN_SECRET=$OAUTH_SECRET ENV=production --app hb$APP_INDEX
+git push bot_$ITERATION master
 heroku scale bot=1 --app hb$APP_INDEX
