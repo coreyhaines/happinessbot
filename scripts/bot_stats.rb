ac9a9ae904d755f4090bb893d81e43ec679c0b08
coreyhaines <coreyhaines@gmail.com>
2012-01-19 22:00:36 -0600

    Add creation date to bot statistics

--- scripts/bot_stats.rb
+++ scripts/bot_stats.rb
@@ -5,13 +5,13 @@ require 'twitter'
 total_followers_count = 0
 total_statuses_count = 0
 
-puts "Name\t\tFollowers\tStatuses"
+puts "Name\t\tFollowers\tStatuses Start Date"
 7.upto(15) do |index|
   name = "happiness2_u_#{index}"
   u = Twitter.user(name)
   followers_count = u.followers_count
   statuses_count = u.statuses_count
-  puts "#{name}\t#{followers_count}\t\t#{statuses_count}"
+  puts "#{name}\t#{followers_count}\t\t#{statuses_count}\t#{u.created_at.to_date}"
   total_followers_count += followers_count
   total_statuses_count += statuses_count
 end
